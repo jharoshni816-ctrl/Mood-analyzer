84ee815aee3b8fee4742243f716099988e91fd36
jharoshni816-ctrl <jharoshni816@gmail.com>
2026-06-26 09:54:43 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+
+st.title("AI Mood Detector 😊")
+
+user_input = st.text_input("Apna mood likho:")
+
+if user_input:
+    text = user_input.lower()
+    
+    if "happy" in text:
+        st.success("😊 You are Happy!")
+    elif "sad" in text:
+        st.error("😢 You are Sad!")
+    elif "angry" in text:
+        st.warning("😡 You are Angry!")
+    else:
+        st.info("😐 Mood samajh nahi aaya")
